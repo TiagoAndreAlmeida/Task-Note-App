a949f7a0799bc7bbe2d92bb8640685e4fdfc1682
Tiago Almeida <thyagoeap@gmail.com>
2020-02-23 13:36:06 -0300

    profile page

--- lib/pages/profile.page.dart
+++ lib/pages/profile.page.dart
@@ -9,7 +9,93 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("Profile"),),
-    );
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+      // mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        SizedBox(height: 80,),
+        Container(
+          // width: 20,
+          height: 120,
+          alignment: Alignment(0.0, 1.5),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage('assets/person.png'),
+              fit: BoxFit.contain,
+            ),
+            border: Border.all(
+              width: 5,
+            ),
+          ),
+          child: Container(
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                width: 2
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(50)
+              )
+            ),
+            child: SizedBox.expand(
+              child: FlatButton(
+                  padding: EdgeInsets.all(0),
+                  child: Icon(
+                    Icons.add,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  onPressed: () => {},
+                )
+              ),
+            ),
+        ),
+        SizedBox(height: 20,),
+        SizedBox(
+          width: double.infinity,
+          child: Text(
+            "User name will",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          )
+        ),
+        SizedBox(height: 5,),
+        SizedBox(
+          width: double.infinity,
+          child: Text(
+            "User email@email.com",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          )
+        ),
+        SizedBox(height: 40,),
+        FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8)
+          ),
+          padding: EdgeInsets.all(6),
+          color: Colors.blue,
+          child: SizedBox(
+            width: double.infinity,
+            child: Text(
+              "Troca de senha",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            )),
+          onPressed: () => print("OK")
+        ),
+      ],
+    ));
   }
 }
